37f28436ed26ac2895daa107371e6c88ecb2f092
ashlamp08 <dhungana.ashok08@gmail.com>
2021-12-02 19:30:48 +0200

    Now lets fix parameters

--- data/data_preprocessor.py
+++ data/data_preprocessor.py
@@ -1,13 +1,14 @@
 import csv
+from random import choices
 
-countries = ["Finland", "Germany", "South Korea", "Nepal", "United States"]
+countries = ["Finland", "Germany", "Hong Kong", "Japan", "Portugal"]
 
 population = {
     "Finland": 5528737,
     "Germany": 83240000,
-    "South Korea": 51780000,
-    "Nepal": 29140000,
-    "United States": 329500000,
+    "Hong Kong": 7482000,
+    "Japan": 125800000,
+    "Portugal": 10310000,
 }
 
 our_data = {}
@@ -23,12 +24,26 @@ with open("cumulative-covid-vaccinations.csv", newline="") as csvfile:
                 our_data[row[0]] = [row[3]]
 
 
+# cleaning our data so it has the  same length
+m = 100000000000
+for c in our_data:
+    m = min(m, len(our_data[c]))
+
+for i in range(len(our_data)):
+    temp = choices(our_data[countries[i]], k = m)
+    temp.sort()
+    our_data[countries[i]] = temp
+
+
 for c in countries:
     with open(c + "_output.csv", "w") as outputfile:
         spamwriter = csv.writer(outputfile, delimiter=",")
         size = len(our_data[c])
         i = 0
-        for x in our_data[c]:
+        temp = our_data[c]
+        temp = [int(x) for x in temp]
+        temp.sort()
+        for x in temp:
             r = [c, i / size, int(x) / (2 * population[c])]
             spamwriter.writerow(r)
             i = i + 1
